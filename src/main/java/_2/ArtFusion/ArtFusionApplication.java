95bac1d675659cb89c38934832a95540e0929fcc
hyunmin504 <hyunmin504@naver.com>
2024-06-19 00:04:07 +0900

    first-commit

---
+++ src/main/java/_2/ArtFusion/ArtFusionApplication.java
@@ -0,0 +1,13 @@
+package _2.ArtFusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtFusionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtFusionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/_2/ArtFusion/ArtFusionApplicationTests.java
@@ -0,0 +1,13 @@
+package _2.ArtFusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtFusionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
