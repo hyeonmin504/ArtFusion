deb6498f07258963f2f6a782dff80333042165b4
hyunmin504 <hyunmin504@naver.com>
2024-06-24 22:47:53 +0900

    예외 코드 EmptyResultDataAccessException.class 추가

--- src/main/java/_2/ArtFusion/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/_2/ArtFusion/exception/handler/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package _2.ArtFusion.exception.handler;
 import _2.ArtFusion.controller.ResponseForm;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MissingServletRequestParameterException;
@@ -24,17 +25,18 @@ public class GlobalExceptionHandler {
     @ExceptionHandler({IllegalArgumentException.class,
             MethodArgumentTypeMismatchException.class,
             MissingServletRequestParameterException.class,
-            ConstraintViolationException.class})
+            ConstraintViolationException.class,
+            EmptyResultDataAccessException.class
+    })
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ResponseForm<Map<String, Object>>> handleBadRequest(Exception ex, WebRequest request) {
         Map<String, Object> body = new HashMap<>();
         body.put("timestamp", LocalDateTime.now());
         body.put("status", HttpStatus.BAD_REQUEST.value());
         body.put("error", "Bad Request");
-        body.put("message", ex.getMessage());
         body.put("path", request.getDescription(false).replace("uri=", ""));
 
-        ResponseForm<Map<String, Object>> responseForm = new ResponseForm<>(HttpStatus.BAD_REQUEST, body, ex.getMessage());
+        ResponseForm<Map<String, Object>> responseForm = new ResponseForm<>(HttpStatus.BAD_REQUEST, body, "올바른 입력값이 아닙니다");
         log.info("error={}",ex);
         return new ResponseEntity<>(responseForm, HttpStatus.BAD_REQUEST);
     }
@@ -46,10 +48,9 @@ public class GlobalExceptionHandler {
         body.put("timestamp", LocalDateTime.now());
         body.put("status", HttpStatus.NOT_FOUND.value());
         body.put("error", "Not Found");
-        body.put("message", ex.getMessage());
         body.put("path", request.getDescription(false).replace("uri=", ""));
 
-        ResponseForm<Map<String, Object>> responseForm = new ResponseForm<>(HttpStatus.NOT_FOUND, body, ex.getMessage());
+        ResponseForm<Map<String, Object>> responseForm = new ResponseForm<>(HttpStatus.NOT_FOUND, body, "올바른 경로가 아닙니다");
         log.info("error={}",ex);
         return new ResponseEntity<>(responseForm, HttpStatus.NOT_FOUND);
     }
@@ -61,7 +62,6 @@ public class GlobalExceptionHandler {
         body.put("timestamp", LocalDateTime.now());
         body.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
         body.put("error", "Internal Server Error");
-        body.put("message", "An unexpected error occurred."); // 사용자에게 일반적인 메시지로 변경
         body.put("path", request.getDescription(false).replace("uri=", ""));
 
         ResponseForm<Map<String, Object>> responseForm = new ResponseForm<>(HttpStatus.INTERNAL_SERVER_ERROR, body, "An unexpected error occurred.");
