af4e3637713a743d11f21f772b89af3ba8206972
hyunmin504 <hyunmin504@naver.com>
2024-06-25 23:54:22 +0900

    스토리보드 작성 후 이미지 생성 mvc api 개발 테스트 이미지 데이터 삽입 완

--- src/main/java/_2/ArtFusion/domain/scene/TemporaryPhotoStorage.java
+++ src/main/java/_2/ArtFusion/domain/scene/TemporaryPhotoStorage.java
@@ -1,9 +1,14 @@
 package _2.ArtFusion.domain.scene;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Getter
 public class TemporaryPhotoStorage {
     @Id @GeneratedValue
