5c5225627db321440676f30855eb7483e2c9c8d1
hyunmin504 <hyunmin504@naver.com>
2024-06-28 04:21:43 +0900

    이미지 생성 api 및 S3 연동

--- src/main/java/_2/ArtFusion/ArtFusionApplication.java
+++ src/main/java/_2/ArtFusion/ArtFusionApplication.java
@@ -1,13 +1,27 @@
 package _2.ArtFusion;
 
+import jakarta.servlet.MultipartConfigElement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.unit.DataSize;
+
+
 
 @SpringBootApplication
 public class ArtFusionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ArtFusionApplication.class, args);
+	}
+
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		factory.setMaxFileSize(DataSize.ofMegabytes(10)); // 최대 파일 크기 설정
+		factory.setMaxRequestSize(DataSize.ofMegabytes(10)); // 최대 요청 크기 설정
+		return factory.createMultipartConfig();
 	}
 
 }
---
+++ src/main/java/_2/ArtFusion/controller/generateStoryController/GenerateStoryController.java
@@ -0,0 +1,45 @@
+package _2.ArtFusion.controller.generateStoryController;
+
+import _2.ArtFusion.controller.ResponseForm;
+import _2.ArtFusion.domain.storyboard.StoryBoard;
+import _2.ArtFusion.exception.NotFoundContentsException;
+import _2.ArtFusion.exception.NotFoundUserException;
+import _2.ArtFusion.repository.StoryBoardRepository;
+import _2.ArtFusion.service.ImageService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/api")
+public class GenerateStoryController {
+
+    private final ImageService imageService;
+    private final StoryBoardRepository storyBoardRepository;
+
+    @PostMapping("/story/generate")
+    public ResponseForm getFinalStory(@RequestParam Long storyId, @RequestParam MultipartFile image) {
+        try {
+            //사용자 체크
+
+            StoryBoard storyBoard = storyBoardRepository.findById(storyId).orElseThrow(
+                    () -> new NotFoundContentsException("해당 컨텐츠를 찾을 수 없습니다")
+            );
+            imageService.uploadImage(image,storyBoard);
+
+            return new ResponseForm<>(HttpStatus.OK, null,"이미지 저장 완료");
+        } catch (NotFoundUserException e) {
+            return new ResponseForm<>(HttpStatus.UNAUTHORIZED, null,"이미지 저장 완료");
+        } catch (IOException e) {
+            return new ResponseForm<>(HttpStatus.SERVICE_UNAVAILABLE, null,"저장중 오류가 발생했습니다");
+        }
+    }
+}
