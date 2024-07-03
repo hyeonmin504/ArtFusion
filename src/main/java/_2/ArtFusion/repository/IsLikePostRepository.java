a2f399fb8e510dc06b4356f500fc40472c2e6411
wookd0ng <wook5307@gmail.com>
2024-07-03 17:41:04 +0900

    feat: 게시글 likes 기능 구현

---
+++ src/main/java/_2/ArtFusion/repository/IsLikePostRepository.java
@@ -0,0 +1,7 @@
+package _2.ArtFusion.repository;
+
+import _2.ArtFusion.domain.archive.IsLikePost;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IsLikePostRepository extends JpaRepository<IsLikePost,Long> {
+}
