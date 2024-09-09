ad8ac8318e94771b3c8c1d68a3cf7b926e937913
wookd0ng <wook5307@gmail.com>
2024-09-09 15:32:28 +0900

    add : 사용자 토큰 추가

--- src/main/java/_2/ArtFusion/repository/r2dbc/SceneFormatR2DBCRepository.java
+++ src/main/java/_2/ArtFusion/repository/r2dbc/SceneFormatR2DBCRepository.java
@@ -1,6 +1,7 @@
 package _2.ArtFusion.repository.r2dbc;
 
 import _2.ArtFusion.domain.r2dbcVersion.SceneFormat;
+import _2.ArtFusion.domain.r2dbcVersion.User;
 import _2.ArtFusion.domain.storyboard.Style;
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
@@ -9,4 +10,11 @@ import reactor.core.publisher.Mono;
 public interface SceneFormatR2DBCRepository extends ReactiveCrudRepository<SceneFormat, Long> {
     @Query("select s.style from story_board s where s.story_id = :storyId")
     Mono<String> findStyleById(Mono<Long> storyId);
+
+    @Query("SELECT u.* " +
+            "FROM user u " +
+            "JOIN story_board sb ON u.id = sb.user_id " +
+            "JOIN scene_format sf ON sb.story_id = sf.story_id " +
+            "WHERE sf.scene_id = :sceneFormatId")
+    Mono<User> findUserBySceneFormatId(Mono<Long> sceneFormatId);
 }
